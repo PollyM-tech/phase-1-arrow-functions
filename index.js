e99308c66cdec2fc49e3e3943022005dd481eb3c
Pauline Moraa <pmoraa431@gmail.com>
2025-03-06 20:22:20 +0300

    arrow functions

---
+++ index.js
@@ -0,0 +1,21 @@
+//1. division
+const divide = function(a, b) {
+    return a / b;
+  };
+  console.log(2000, 100);
+  //divide divides 2000 by 100"
+ 
+
+
+
+
+
+
+
+
+
+  // 2. Arrow function for square
+  const square = (x) => x * x;
+  
+  // 3. Arrow function for add
+  const add = (a, b) => a + b;
